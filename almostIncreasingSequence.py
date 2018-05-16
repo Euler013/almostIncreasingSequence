ff591670f1d73616abf5e3207232489990a28374
Euler <pablo_ezequiel4@hotmail.com>
2018-05-16 18:43:57 -0300

    Second Try

--- almostIncreasingSequence.py
+++ almostIncreasingSequence.py
@@ -14,9 +14,11 @@ You can remove 3 from the array to get the strictly increasing sequence [1, 2]. 
 
 def almostIncreasingSequence(sequence):
     noIncreasing = 0
-    for i in range(len(sequence)-1):
-        if sequence[i] > sequence[i+1]:
-            noIncreasing += 1
+    length = len(sequence)
+    for i in range(length-1):
+        for j in range(i+1, length):
+            if sequence[i] > sequence[j]:
+                noIncreasing += 1
 
     if noIncreasing > 1:
     	return False
